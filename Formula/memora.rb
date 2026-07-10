6ef876ebfccd76049afe787db8b9dc10ce8d3222
Rado <r.tsvetkov@hotmail.com>
2026-07-10 16:31:36 +0200

    memora 0.2.1

--- Formula/memora.rb
+++ Formula/memora.rb
@@ -5,23 +5,23 @@ class Memora < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/radotsvetkov/memora/releases/download/v0.1.29/memora-cli-aarch64-apple-darwin.tar.xz"
-      sha256 "8e4bd44131a97586832484223959258665b7fe537765f5933f1f9018e9e13a21"
+      url "https://github.com/radotsvetkov/memora/releases/download/v0.2.1/memora-cli-aarch64-apple-darwin.tar.xz"
+      sha256 "9f8ca045ec3c2796816d3b9f929166211b55849ff0a202592857fa632b249ed2"
     end
     on_intel do
-      url "https://github.com/radotsvetkov/memora/releases/download/v0.1.29/memora-cli-x86_64-apple-darwin.tar.xz"
-      sha256 "80fc565a26ea2849249a75ef98fe56fad0da9176b7dc932cd32a4cea8e20b999"
+      url "https://github.com/radotsvetkov/memora/releases/download/v0.2.1/memora-cli-x86_64-apple-darwin.tar.xz"
+      sha256 "8c6e9cee2a07063e1bc58b4345d8d71172794e376c84473ed7f98724fe32970d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/radotsvetkov/memora/releases/download/v0.1.29/memora-cli-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "bc2be7f8efa71309e8e13592b42c11b2845b4fca6946b53cd719a3aaa8790de7"
+      url "https://github.com/radotsvetkov/memora/releases/download/v0.2.1/memora-cli-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "e0e88f882656fc6df154f02751c979567900aa1e846df96352c50097e25814b3"
     end
     on_intel do
-      url "https://github.com/radotsvetkov/memora/releases/download/v0.1.29/memora-cli-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "df195df7a146db231ef019369c08325f6e5a6a9e76812a5dcb81ffa246ada463"
+      url "https://github.com/radotsvetkov/memora/releases/download/v0.2.1/memora-cli-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "d507f31f724eb811324f16ff0d9b3359c76a82d481b2fabdc349f059c6fc8fda"
     end
   end
 
